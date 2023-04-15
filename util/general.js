eb90d22b44498414f4e568ccf1d6c6ea2f049956
Yuuto <thelio.trinite@outlook.com>
2023-04-15 14:24:04 +0200

    running from exe

--- util/general.js
+++ util/general.js
@@ -1,3 +1,7 @@
 module.exports.sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
+
+module.exports.runningFromExecutable = () => {
+  return process.argv0.endsWith('.exe') && !process.argv0.includes('node.exe');
+};
