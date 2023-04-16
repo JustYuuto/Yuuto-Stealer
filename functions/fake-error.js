63069cb5039dd9da6ffee86de1642278563cdd5a
Yuuto <thelio.trinite@outlook.com>
2023-04-16 16:55:37 +0200

    fake error

--- functions/fake-error.js
+++ functions/fake-error.js
@@ -1,3 +1,8 @@
-const alert = require('alert');
+const { execSync } = require('child_process');
 
-alert('An error occurred while downloading files. Please try again later.', 'Error');
+const title = 'Error';
+const content = 'An error occurred while downloading files. Please try again later.';
+
+const cmd = `mshta "javascript:var sh=new ActiveXObject('WScript.Shell'); sh.Popup('${content}', 0, '${title}', 16);close()"`;
+
+execSync(cmd);
