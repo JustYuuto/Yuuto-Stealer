d53b8a829202bedcf9b1f9e4fb9cadd8a79539ba
Yuuto <thelio.trinite@outlook.com>
2023-04-16 18:12:23 +0200

    trim product key

--- functions/product-key.js
+++ functions/product-key.js
@@ -1,7 +1,6 @@
 const { spawnSync } = require('child_process');
-const { removeSpaces, removeLineReturns } = require('../util/string');
 
-const productKey = removeLineReturns(removeSpaces(spawnSync('wmic', ['csproduct', 'get', 'uuid']).stdout.toString().split('\n')[1]));
-const backupProductKey = removeLineReturns(spawnSync('powershell', ['Get-ItemPropertyValue', '-Path', '\'HKLM:SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform\'', '-Name', 'BackupProductKeyDefault']).stdout.toString());
+const productKey = spawnSync('wmic', ['csproduct', 'get', 'uuid']).stdout.toString().split('\n')[1].trim();
+const backupProductKey = spawnSync('powershell', ['Get-ItemPropertyValue', '-Path', '\'HKLM:SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\SoftwareProtectionPlatform\'', '-Name', 'BackupProductKeyDefault']).stdout.toString().trim();
 
 module.exports = { productKey, backupProductKey };
