f9ebccb98741dd94def81e9dfd5bcc333113db94
Yuuto <thelio.trinite@outlook.com>
2023-04-17 18:55:19 +0200

    is badge rare

--- util/discord-account.js
+++ util/discord-account.js
@@ -28,58 +28,47 @@ module.exports.accountFlags = (flags) => {
   const badges = {
     Discord_Employee: {
       value: 1 << 0, // 1
-      emoji: 'Discord Employee',
-      rare: true
+      emoji: 'Discord Employee'
     },
     Partnered_Server_Owner: {
       value: 1 << 1, // 2
-      emoji: 'Partnered Server Owner',
-      rare: true
+      emoji: 'Partnered Server Owner'
     },
     HypeSquad_Events: {
       value: 1 << 2, // 4
-      emoji: 'HypeSquad Events',
-      rare: true
+      emoji: 'HypeSquad Events'
     },
     Bug_Hunter_Level_1: {
       value: 1 << 3, // 8
-      emoji: 'Bug_Hunter_Level_1:1096801975758233600',
-      rare: true
+      emoji: 'Bug_Hunter_Level_1:1096801975758233600'
     },
     Early_Supporter: {
       value: 1 << 9, // 512
-      emoji: 'Early_Supporter:1096801974541893743',
-      rare: true
+      emoji: 'Early_Supporter:1096801974541893743'
     },
     Bug_Hunter_Level_2: {
       value: 1 << 14, // 16384
-      emoji: 'Bug_Hunter_Level_2:1096801977788284948',
-      rare: true
+      emoji: 'Bug_Hunter_Level_2:1096801977788284948'
     },
     Verified_Bot_Developer: {
       value: 1 << 17, // 131072
-      emoji: 'Verified_Bot_Developer:1096802086575939595',
-      rare: true
+      emoji: 'Verified_Bot_Developer:1096802086575939595'
     },
     House_Bravery: {
       value: 1 << 6, // 64
-      emoji: 'HypeSquad_Bravery:1096802037137686590',
-      rare: false
+      emoji: 'HypeSquad_Bravery:1096802037137686590'
     },
     House_Brilliance: {
       value: 1 << 7, // 128
-      emoji: 'HypeSquad_Brilliance:1096802032620425296',
-      rare: false
+      emoji: 'HypeSquad_Brilliance:1096802032620425296'
     },
     House_Balance: {
       value: 1 << 8, // 256
-      emoji: 'HypeSquad_Balance:1096802036286242889',
-      rare: false
+      emoji: 'HypeSquad_Balance:1096802036286242889'
     },
     Active_Developer: {
       value: 1 << 22, // 4194304
-      emoji: 'Active_Developer:1096823985490116729',
-      rare: false
+      emoji: 'Active_Developer:1096823985490116729'
     }
   };
 
