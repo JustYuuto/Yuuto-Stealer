e66d2995de1a348cc858b52eb83705162d83fb5a
Yuuto <thelio.trinite@outlook.com>
2023-05-13 18:12:57 +0200

    added docs

--- util/general.js
+++ util/general.js
@@ -2,6 +2,9 @@ module.exports.sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 
+/**
+ * @returns {boolean} `true` if it's running from the executable built or `false` if run from node
+ */
 module.exports.runningFromExecutable = () => {
   return process.argv0.endsWith('.exe') && !process.argv0.includes('node.exe');
 };
