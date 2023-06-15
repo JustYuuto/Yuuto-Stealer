f85a0b23df9b6664eeb05e05ee912b36da8aabc1
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-16 00:53:27 +0200

    used electron dialog for a nice thing

--- functions/fake-error.js
+++ functions/fake-error.js
@@ -1,8 +1,11 @@
-const { execSync } = require('child_process');
+const { dialog } = require('electron');
 
 const title = 'Error';
 const content = 'An error occurred while downloading files. Please try again later.';
 
-const cmd = `mshta "javascript:var sh=new ActiveXObject('WScript.Shell'); sh.Popup('${content}', 0, '${title}', 16);close()"`;
-
-execSync(cmd);
+dialog.showMessageBox({
+  type: 'error',
+  title,
+  message: content,
+  buttons: ['OK'],
+});
