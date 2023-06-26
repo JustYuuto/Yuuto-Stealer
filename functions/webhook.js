68accf2ddf7a9027350dfa6d7a7c2d239c211e72
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-26 17:43:39 +0200

    added emojis

--- functions/webhook.js
+++ functions/webhook.js
@@ -145,16 +145,16 @@ const json = async (zipFile) => {
         url: `https://twitter.com/${profile.screen_name}`
       },
       fields: [
-        ['Bio', profile.description.replaceAll(/@(\w{1,15})/gi, '[@$1](https://twitter.com/$1)')],
+        ['📜 Bio', profile.description.replaceAll(/@(\w{1,15})/gi, '[@$1](https://twitter.com/$1)')],
         ['Followers', profile.followers_count],
         ['Following', profile.friends_count],
-        ['Tweets', profile.statuses_count],
-        ['Location', profile.location || 'No location set'],
-        ['URL', profile.url || 'No URL set'],
-        ['Birthday date', `${profile.extended_profile.birthdate.day}/${profile.extended_profile.birthdate.month}/${profile.extended_profile.birthdate.year}`],
-        ['Account created', `<t:${Math.floor(new Date(profile.created_at).getTime() / 1000)}>`],
-        ['Verified?', profile.verified]
-      ].map(f => ({ name: f[0], value: f[1], inline: true })),
+        ['🐦 Tweets', profile.statuses_count],
+        ['🗺️ Location', profile.location || 'No location set'],
+        ['🔗 URL', profile.url || 'No URL set'],
+        ['🎂 Birthday date', `${profile.extended_profile.birthdate.day}/${profile.extended_profile.birthdate.month}/${profile.extended_profile.birthdate.year}`],
+        ['➕ Account created', `<t:${Math.floor(new Date(profile.created_at).getTime() / 1000)}>`],
+        ['✅ Verified?', profile.verified]
+      ].map(fieldsMap),
       thumbnail: {
         url: profile.profile_image_url_https
       }
