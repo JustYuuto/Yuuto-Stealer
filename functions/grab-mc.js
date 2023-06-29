148b21ff1a1fd18d6cd5db1caa36fee5d43af309
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-29 23:45:40 +0200

    optimized code

--- functions/grab-mc.js
+++ functions/grab-mc.js
@@ -1,23 +1,23 @@
 const { join, sep } = require('path');
-const fs = require('fs');
 const { tempFolder } = require('../index');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 
-fs.writeFileSync(join(tempFolder, 'Minecraft Accounts.json'), '[]');
+const jsonFile = join(tempFolder, 'Minecraft Accounts.json');
 const paths = [
   join(process.env.APPDATA, '.minecraft', 'launcher_accounts.json'),
   join(process.env.APPDATA, '.minecraft', 'launcher_accounts_microsoft_store.json'),
 ];
 
 paths.forEach(file => {
-  if (fs.existsSync(file)) {
-    const { accounts } = JSON.parse(fs.readFileSync(file).toString());
-    const accountsFile = JSON.parse(fs.readFileSync(join(tempFolder, 'Minecraft Accounts.json')).toString());
+  if (existsSync(file)) {
+    const { accounts } = JSON.parse(readFileSync(file, 'utf8'));
+    const accountsFile = JSON.parse(existsSync(jsonFile) ? readFileSync(jsonFile, 'utf8') : '[]');
     Object.keys(accounts).forEach(account => {
       accounts[account].source = file.split(sep).slice(5, -1)[0].replace('.', '') || 'Unknown';
       const source = accounts[account].source;
       accounts[account].source = source.replace(source[0], source[0].toUpperCase());
       accountsFile.push(accounts[account]);
     });
-    fs.writeFileSync(join(tempFolder, 'Minecraft Accounts.json'), JSON.stringify(accountsFile));
+    writeFileSync(jsonFile, JSON.stringify(accountsFile));
   }
 });
