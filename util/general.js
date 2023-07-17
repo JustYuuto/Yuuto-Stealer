6e167c3bfc9d0db7df824bbd6f1dca864f39a4cb
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-17 12:00:07 +0200

    util: running from startup

--- util/general.js
+++ util/general.js
@@ -8,3 +8,10 @@ module.exports.sleep = (ms) => {
 module.exports.runningFromExecutable = () => {
   return process.argv0.endsWith('.exe') && !process.argv0.includes('node.exe') && !process.argv0.includes('electron.exe');
 };
+
+/**
+ * @return {boolean} `true` if the program is run from the startup programs, or `false` if not
+ */
+module.exports.runningFromStartup = () => {
+  return require('os').uptime() < (60_000 * 5);
+};
