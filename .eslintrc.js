e6e79709b72e7b614b05d2a096910d9191f3855c
JustYuuto <55290516+JustYuuto@users.noreply.github.com>
2023-07-21 16:04:35 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
   },
   'rules': {
     'indent': ['error', 2, { 'SwitchCase': 1 }],
-    'linebreak-style': ['error', 'windows'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always']
   }
