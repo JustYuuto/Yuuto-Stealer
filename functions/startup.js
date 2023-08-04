87846a7c3df1adc9a184b4bbe9486ee1db3d7e34
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-05 00:43:31 +0200

    fix startup executable

--- functions/startup.js
+++ functions/startup.js
@@ -1,10 +1,8 @@
 const { app } = require('electron');
-const path = require('path');
-const exeName = path.basename(process.execPath);
 
 app.setLoginItemSettings({
   openAtLogin: true,
   openAsHidden: true,
   enabled: true,
-  path: exeName,
+  path: process.execPath
 });
